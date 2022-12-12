5b065bf8cb211c9855ecd8e9c2f7acbfd3d8ca5a
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2022-12-12 16:26:25 +0200

    create modal

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -81,6 +81,7 @@ class App extends Component {
           />
         )}
         {images.length > 0 && <Button onClick={this.loadMoreSubmit} />}
+
         <Toaster position="top-right" reverseOrder={false} />
       </Container>
     );
